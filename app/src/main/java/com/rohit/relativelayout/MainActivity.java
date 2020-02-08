08d9b3014052ec558b53ac2311a7d01fcf41878e
Rohit kumar <rohit9075@gmail.com>
2020-02-08 11:24:49 +0530

    Handling Events using Java

--- app/src/main/java/com/rohit/relativelayout/MainActivity.java
+++ app/src/main/java/com/rohit/relativelayout/MainActivity.java
@@ -2,12 +2,48 @@ package com.rohit.relativelayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText1, editText2;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //intiView method call
+        initView();
     }
+
+
+    //initView method initialization
+    private void initView(){
+        editText1 = findViewById(R.id.edittext);
+        editText2 = findViewById(R.id.edittext1);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = String.valueOf(editText1.getText());
+                String email = String.valueOf(editText2.getText());
+
+                Toast.makeText(MainActivity.this, "Name is : " + name + "Email is : " + email, Toast.LENGTH_SHORT).show();
+                Log.d("MyLog", "Name is : " + name);
+                Log.d("MyLog", "Email is : " + email);
+
+            }
+        });
+    }
+
+
+
 }
